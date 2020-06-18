0832778ee3e861f549741e15a39e84f4c8941028
Mike Green <30730776+developermgreen@users.noreply.github.com>
2020-06-18 11:31:14 -0400

    Add files via upload

---
+++ house-basic.js
@@ -0,0 +1,85 @@
+// 1. Write a line of code to check if script is working, connect this file, then verify the message appears in the console log.
+
+
+
+
+
+
+
+
+
+//2. Write a lines of code to select the first bulb (and verify). When working, select the other bulbs.
+//HINT: const variable = document.querySelector("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//3. Add an event listeners and function (e) for the first bulb. When working, add to the other bulbs.
+//HINT: variable.addEventListener('eventtype', (e)=>{actions})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//4. Let's turn the lights on add to #3. Console log that object's classList.
+//HINT: variable.classList.add("class")
+
+
+
+
+
+
+//5. Comment out code that turns bulbs on in #3. Add a line and comment what that code does.
+//Let's turn all the lights off. Add to function #3.
+//HINT: variable.classList.remove("class")
+
+
+
+
+
+
+//6. Comment out code that turns bulbs off in #3. Add a line and comment what that code does.
+//Let's turn the lights off/on. Add to function in #3.
+//HINT: variable.classList.toggle("class")
+
+
+
+
+
+
+
+//7. Create a universal function to handle the click. Move function to top of file.
+//Comment out all of #3 and code below.
+//HINT (function): const functionName = (e) =>{e.target.classList.toggle("active")}
+//HINT (varibles): variable.addEventListener('click',functionName)
---
+++ house-mega.js
@@ -0,0 +1,24 @@
+// 1. Write a line of code to check if script is working, connect this file, then verify the message appears in the console log.
+
+
+
+
+
+
+// 2. Write lins of code to select all bulbs.
+// HINT: let variable = document.querySelectorAll(.class)
+
+
+
+
+
+
+// 3. Use the forEach(item) function to console.log the each bulb's id)
+//HINT: variable.forEach((item) =>{console.log(item.id)})
+
+
+
+
+
+
+//4. Add code to #3 to toggle the lights off and on.
